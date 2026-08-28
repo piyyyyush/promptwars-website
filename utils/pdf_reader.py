e9302e025d8af1595b09bda7116089dcfa08879e
piyyyyush <piyyushjakhar@gmail.com>
2026-08-28 14:47:24 +0530

    Update pdf_reader.py

--- utils/pdf_reader.py
+++ utils/pdf_reader.py
@@ -1,1 +1,10 @@
-#pdf_reader
+import PyPDF2
+
+def extract_text(file):
+    reader = PyPDF2.PdfReader(file)
+    text = ""
+
+    for page in reader.pages:
+        text += page.extract_text() or ""
+
+    return text
